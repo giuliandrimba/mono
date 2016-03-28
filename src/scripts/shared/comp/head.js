24b6ec0f79ceae4af325ccfb8a2d6be8a2593d21
Giulian Drimba <giuliandrimba@gmail.com>
2016-03-28 19:06:53 -0300

    Setup mokney threejs scene

---
+++ src/scripts/shared/comp/head.js
@@ -0,0 +1,13 @@
+class Head {
+  constructor() {
+
+  }
+
+  animationIn() {
+
+  }
+
+  animationOut() {
+    
+  }
+}
--- src/scripts/views/monkey.js
+++ src/scripts/views/monkey.js
@@ -1,7 +1,12 @@
 import tmpl from "templates/views/monkey.html";
 import parseHTML from "scripts/shared/lib/parseHTML";
+import THREE from "three";
+import OBJLoader from "three-obj-loader";
 
 var el = undefined;
+var scene = undefined;
+var camera = undefined;
+var renderer = undefined;
 
 export function intro(req, done) {
   render();
@@ -15,13 +20,54 @@ export function outro(req, done) {
 function animationin() {
 }
 
+function events() {
+  window.addEventListener("resize", resize);
+}
+
 function render() {
   el = parseHTML(tmpl);
   document.getElementById("pages").appendChild(el);
 
+  scene = new THREE.Scene()
+  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+  renderer = new THREE.WebGLRenderer({alpha: true})
+
+  camera.position.set(0, 0, 4)
+  scene.fog = new THREE.Fog(0x222222, 20, -20);
+
+  addLights();
+
+  el.appendChild(renderer.domElement);
+
   animationin();
+  resize()
+  events()
+  loop()
+}
+
+function addLights() {
+  var lights = [];
+  lights[0] = new THREE.PointLight( 0xffffff, 1, 0 );
+  lights[1] = new THREE.PointLight( 0xffffff, 1, 0 );
+  lights[2] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+  lights[1].position.set( 100, 200, 100 );
+  lights[2].position.set( -100, -200, -100 );
+
+  scene.add( lights[0] );
+  scene.add( lights[1] );
+  scene.add( lights[2] );
 }
 
 function resize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function loop() {
+  requestAnimationFrame( loop );
+  camera.lookAt( scene.position )
+  renderer.render(scene, camera);
 }
