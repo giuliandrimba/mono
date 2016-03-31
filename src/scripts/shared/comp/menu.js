d8456cbbadcaa4f1f7035f050ee2da8e6469b182
Giulian Drimba <giuliandrimba@gmail.com>
2016-03-31 15:05:31 -0300

    Develop menu hover states

--- src/scripts/shared/comp/menu.js
+++ src/scripts/shared/comp/menu.js
@@ -3,6 +3,7 @@ import Ease from "d3-ease";
 import Vivus from "vivus";
 import Snap from "snapsvg";
 import Segment from "segment-js";
+import _ from "lodash";
 
 export default class Menu {
   constructor() {
@@ -23,6 +24,9 @@ export default class Menu {
     this.innerPath = this._createInnerPath();
     this.segmentInnerPath = new Segment(this.innerPath.node);
 
+    this.overPath = this._createOverPath();
+    this.segmentOverPath = new Segment(this.overPath.node);
+
     this.circle = this._createCircle();
 
     this.SVG.append(this.borderPath);
@@ -30,18 +34,42 @@ export default class Menu {
     this.SVG.append(this.circle);
 
     parent.appendChild(this.SVG.node);
+    this.events();
+  }
+
+  events() {
+    this.SVG.node.addEventListener("mouseover", this.onMouseOver.bind(this))
+    this.SVG.node.addEventListener("mouseout", this.onMouseOut.bind(this))
+  }
+
+  onMouseOver() {
+    TweenMax.to(this.innerPath.node, 1, {strokeDasharray: this.segmentInnerPath.strokeDasharray("0%", "0%"), ease:Expo.easeOut });
+    TweenMax.to(this.overPath.node, 0.5, {strokeDasharray: this.segmentOverPath.strokeDasharray("0%", "100%"), ease:Expo.easeOut });
+    this.circle.animate({cy:this.SIZE / 2, cx:this.SIZE / 2, r:26, fill:"#ea232a"}, 750, Ease.easeExpOut)
+  }
+
+  onMouseOut() {
+    TweenMax.to(this.overPath.node, 0.75, {strokeDasharray: this.segmentOverPath.strokeDasharray("0%", "0%"), ease:Expo.easeOut });
+    TweenMax.to(this.innerPath.node, 0.75, {strokeDasharray: this.segmentInnerPath.strokeDasharray("0%", "100%"), ease:Expo.easeOut });
+    this.circle.animate({cy:30, cx:86, r:13, fill:"#85734c"}, 750, Ease.easeExpOut)
   }
 
   beforeAnimationIn() {
     TweenMax.set(this.borderPath.node, { css:{strokeDasharray: this.segmentBorderPath.strokeDasharray(0, 0) } });
     TweenMax.set(this.innerPath.node, { css:{strokeDasharray: this.segmentInnerPath.strokeDasharray(0, 0) } });
+    TweenMax.set(this.overPath.node, { css:{strokeDasharray: this.segmentOverPath.strokeDasharray(0, 0) } });
   }
 
   animationIn() {
     this.beforeAnimationIn();
 
     TweenMax.to(this.borderPath.node, 0.75, {strokeDasharray: this.segmentBorderPath.strokeDasharray("0%", "100%"), delay:0.25, ease:Expo.easeIn });
-    TweenMax.to(this.innerPath.node, 1.5, {strokeDasharray: this.segmentInnerPath.strokeDasharray("0%", "100%"), delay:1, ease:Expo.easeOut });
+    TweenMax.to(this.innerPath.node, 1.3, {strokeDasharray: this.segmentInnerPath.strokeDasharray("0%", "100%"), delay:1, ease:Expo.easeOut });
+
+    _.delay(()=>{
+      this.circle.animate({cy:30}, 750, Ease.easeExpOut)
+      TweenMax.to(this.circle.node, 0.75, {cy: 30, ease:Expo.easeIn });
+    }, 2000)
   }
 
   _createCircle() {
@@ -65,7 +93,7 @@ export default class Menu {
     let face = this.SVG.path(path.getPath())
     face.attr({
       id: "menu:out",
-      stroke: "#85734c",
+      stroke: "#4d4d4d",
       strokeWidth: "1" ,
       fill: "none"
     })
@@ -83,7 +111,23 @@ export default class Menu {
     let face = this.SVG.path(path.getPath())
     face.attr({
       id: "menu:inner",
-      stroke: "#85734c",
+      stroke: "#4d4d4d",
+      strokeWidth: "0.5" ,
+      fill: "none"
+    })
+
+    return face;
+  }
+
+  _createOverPath() {
+    let path = svgPath();
+    path.moveTo(this.SIZE,0);
+    path.lineTo(0,this.SIZE);
+
+    let face = this.SVG.path(path.getPath())
+    face.attr({
+      id: "menu:over",
+      stroke: "#4d4d4d",
       strokeWidth: "0.5" ,
       fill: "none"
     })
