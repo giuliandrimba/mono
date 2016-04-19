a322ddb82dcb657cbb00891894ae32e813d17c1a
Giulian Drimba <giuliandrimba@gmail.com>
2016-04-18 22:21:27 -0300

    Add Calendar view

---
+++ src/scripts/shared/comp/grid.js
@@ -0,0 +1,141 @@
+import moment from "moment";
+window.moment = moment;
+import Caleido from "./caleido";
+import _ from "lodash";
+
+export default class Grid {
+  constructor(parent, month) {
+    this.parent = parent;
+    this.month = month;
+    this.ORIGINAL_WIDTH = 1920;
+    this.ORIGINAL_HEIGHT = 1080;
+    this.TOTAL_DAYS = 15;
+    this.days = [];
+    this.texts = []
+    this.textsContainer = undefined;
+    this.mask = undefined;
+    this.caleido = undefined;
+
+    this.el = undefined;
+    this.buildDays()
+    this.render()
+    if(moment().month() === this.month) {
+      this.addCaleido()
+    }
+  }
+
+  render() {
+    this.el = new PIXI.Container()
+    this.mask = this.buildMask()
+    this.textsContainer = this.buildTexts()
+    this.el.addChild(this.textsContainer);
+    this.el.addChild(this.mask);
+    this.textsContainer.mask = this.mask;
+    this.parent.addChild(this.el);
+  }
+
+  animate() {
+    if(moment().month() === this.month){
+      this.caleido.reset()
+      for(var i = 0; i < this.texts.length; i++) {
+        this.texts[i].y = this.texts[i]._y2
+        TweenMax.to(this.texts[i], 3, {y:this.texts[i]._y, ease:Quart.easeInOut, delay:this.texts[i]._delay * 0.1})
+      }
+
+      _.delay(this.showCaleido.bind(this), 3000)
+    }
+  }
+
+  showCaleido() {
+    this.caleido.show();
+  }
+
+  addCaleido() {
+    this.caleido = new Caleido(moment().date());
+    this.textsContainer.addChild(this.caleido.el);
+    this.caleido.el.x = this.texts[7].x + (this.texts[7].width / 2)
+    this.caleido.el.y = this.texts[7].y + (this.texts[7].height / 2)
+  }
+
+  buildTexts() {
+    var marginLeft = Math.round(200 * window.innerWidth / this.ORIGINAL_WIDTH);
+    var marginBottom = Math.round(200 * window.innerHeight / this.ORIGINAL_HEIGHT);
+    var fontSize = Math.round(120 * window.innerWidth / this.ORIGINAL_WIDTH);
+
+    var col = 0;
+    var row = 0;
+
+    var container = new PIXI.Container()
+
+    for(var i = 0; i <= this.TOTAL_DAYS; i++) {
+
+      let num = this.days[i];
+
+      if(parseInt(num) < 10) {
+        num = `0${num}`
+      }
+
+      let text = new PIXI.Text(num,{font : `${fontSize}px Helvetica`, fill : 0xFFFFFF});
+      text.alpha = 0.3;
+      text.x = col * (marginLeft + text.width);
+      if(moment().month() === this.month){
+        text._y = row * (marginBottom + text.height);
+        text._y2 = text._y + (marginBottom * row * 2)
+        text._delay = row;
+        text.y = text._y
+      } else {
+        text.y = row * (marginBottom + text.height);
+      }
+
+      col++;
+      if(col > 4) {
+        col = 0;
+        row++;
+      }
+
+      container.y = window.innerHeight / 2 - container.height / 2
+      container.x = window.innerWidth / 2 - container.width / 2
+      container.addChild(text);
+      this.texts.push(text);
+    }
+
+    return container;
+  }
+
+  buildDays() {
+
+    var diff = moment().month() - this.month;
+    var date = moment().subtract(14 * diff, 'days')
+
+    this.days[7] = date.date()
+
+    for(var i = 6; i > -1; i--) {
+      var a = date.clone();
+      this.days[i] = a.subtract(7 - i, 'days').date()
+    }
+
+    for(var i = 8; i < this.TOTAL_DAYS; i++) {
+      var a = date.clone();
+      this.days[i] = a.add(i - 7, 'days').date()
+    }
+  }
+
+  buildMask() {
+    var mask = new PIXI.Graphics()
+    mask.beginFill(0xFF0000)
+    mask.drawRect(0,0,window.innerWidth, window.innerHeight)
+    mask.endFill()
+    return mask;
+  }
+
+  resize() {
+    this.texts = [];
+    this.el.removeChildren()
+    this.textsContainer = this.buildTexts()
+    this.el.addChild(this.textsContainer);
+
+    if(moment().month() === this.month) {
+      this.addCaleido()
+    }
+  }
+}
--- src/scripts/views/calendar.js
+++ src/scripts/views/calendar.js
@@ -1,7 +1,68 @@
-export function intro() {
-	
+import TweenMax from "gsap";
+import moment from "moment";
+import Grid from "scripts/shared/comp/grid";
+import _ from "lodash";
+
+var renderer = undefined;
+var stage = undefined;
+var grid = undefined;
+var gridsContainer = undefined;
+var numGrids = 12;
+var numGrids = 12;
+var grids = [];
+
+export function intro(req, done) {
+	render();
+  done();
 }
 
-export function outro() {
-	
+export function outro(req, done) {
+  done();
+}
+
+function render() {
+  renderer = new PIXI.WebGLRenderer(window.innerWidth, window.innerHeight, {antialias: true});
+  renderer.view.id = "calendar";
+  document.getElementById("pages").appendChild(renderer.view);
+
+  stage = new PIXI.Container();
+  gridsContainer = new PIXI.Container;
+
+  for(var i = 0; i < numGrids; i++) {
+    let grid = new Grid(gridsContainer, i)
+    grid.el.y = window.innerHeight * i;
+    grids.push(grid);
+  }
+
+  document.body.addEventListener("mousedown", animationIn)
+  window.addEventListener("resize", resize)
+
+  stage.addChild(gridsContainer);
+
+  animationIn();
+  resize();
+  loop()
+}
+
+function resize() {
+  renderer.resize(window.innerWidth, window.innerHeight);
+
+  for(var i = 0; i < numGrids; i++) {
+    grids[i].resize();
+  }
+}
+
+function animationIn() {
+  gridsContainer.y = 0;
+  let _y = window.innerHeight * moment().month();
+  TweenMax.to(gridsContainer, 3, {y:- _y, ease:Quart.easeInOut});
+  for(var i = 0; i < numGrids; i++) {
+    grids[i].animate();
+  }
+}
+
+function loop() {
+  window.requestAnimationFrame(loop);
+
+  renderer.render(stage);
 }
